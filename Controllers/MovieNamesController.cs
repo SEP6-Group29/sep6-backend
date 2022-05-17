07bcac67a827b121fd769e22466b30e18fa2d117
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2022-05-17 13:25:59 +0200

    Add project files.

---
+++ Controllers/MovieNamesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MovieApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieNamesController : ControllerBase
+    {
+
+        private readonly IConfiguration configuration;
+
+        public MovieNamesController (IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select MovieName from dbo.Movies";
+            DataTable table = new DataTable();
+            string sqlDataSource = configuration.GetConnectionString("MoviesAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
